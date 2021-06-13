cafe3481a4fb6c5a9bdf86086b02937b4d98e4ef
huynhlehoangduc <huynhlehoangduc@gmail.com>
2021-06-13 23:57:03 +0700

    feat: update ui empty for table

--- src/app/features/todo/store/todo.reducers.ts
+++ src/app/features/todo/store/todo.reducers.ts
@@ -3,29 +3,7 @@ import { add, list } from './todo.actions';
 import { Todo } from '../interfaces/todo';
 
 
-export const initialState: Todo[] = [
-    {
-        id: 1,
-        name: 'Todo 1',
-        description: '',
-        isDone: false,
-        isFavourite: false,
-    },
-    {
-        id: 2,
-        name: 'Todo 2',
-        description: '',
-        isDone: true,
-        isFavourite: false,
-    },
-    {
-        id: 3,
-        name: 'Todo 3',
-        description: '',
-        isDone: false,
-        isFavourite: true,
-    },
-];
+export const initialState: Todo[] = [];
 
 export const todoReducer = createReducer(
     initialState,
